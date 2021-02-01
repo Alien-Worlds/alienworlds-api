4b685db1ed2bd44b8382039e54eb50d50d090856
Michael Yeates <michaeljyeates@gmail.com>
2021-02-01 20:22:21 +0000

    Add check config command

---
+++ scripts/check-config.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const fs = require('fs')
+
+const path = './config.js'
+
+try {
+    if (fs.existsSync(path)) {
+        process.exit(0)
+    }
+    else {
+        process.exit(1)
+    }
+} catch(err) {
+    process.exit(1)
+}
