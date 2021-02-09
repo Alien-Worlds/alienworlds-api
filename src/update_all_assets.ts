7b4e9b93674153e35cd1c8a4f2a639fc5b3d0589
Michael Yeates <michaeljyeates@gmail.com>
2021-02-09 18:58:33 +0000

    Allow for large numbers of assets

--- src/update_all_assets.ts
+++ src/update_all_assets.ts
@@ -9,13 +9,15 @@ import {AssetAggregator} from "./include/assetaggregator";
     const mongo: any = await connectMongo(config.mongo);
     console.log('Connected to mongo')
 
-    const asset_ids = await mongo.collection('assets_raw').distinct('asset_id');
-    console.log(asset_ids)
+    // const asset_ids = await mongo.collection('assets_raw').distinct('asset_id');
+    const asset_ids = await mongo.collection('assets_raw').aggregate([{$group: {_id: '$asset_id'} }]);
 
     const aggregator = new AssetAggregator(config, mongo);
-    for (let a = 0; a < asset_ids.length; a++){
-        await aggregator.process(asset_ids[a].toString());
-        console.log(`Processed ${asset_ids[a]}`);
+    while (await asset_ids.hasNext()) {
+        const a = await asset_ids.next();
+        const asset_id = a._id;
+        await aggregator.process(asset_id.toString());
+        console.log(`Processed ${asset_id}`);
     }
 
     process.exit(0);
