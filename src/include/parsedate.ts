a6dcea54cdac34f082ba74b6d06652b2e76643c0
Michael Yeates <michaeljyeates@gmail.com>
2021-05-10 18:26:31 +0100

    Set milliseconds to 0

--- src/include/parsedate.ts
+++ src/include/parsedate.ts
@@ -14,6 +14,7 @@ export function parseDate (fullStr) {
     dt.setUTCHours(hourStr)
     dt.setUTCMinutes(minuteStr)
     dt.setUTCSeconds(secondStr)
+    dt.setUTCMilliseconds(0)
 
     return dt.getTime()
 }
