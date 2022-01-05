6cce3ab82e9e958a6507fe3131037517ffb8df31
rkamysz <radoslaw.kamysz@gmail.com>
2022-01-05 19:51:15 +0100

    force build api tests

--- scripts/run-api-tests.sh
+++ scripts/run-api-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 script="$(dirname $0)/../docker-compose-api-tests.yml"
-docker compose -f $script up --abort-on-container-exit --exit-code-from api-tests
+docker compose -f $script up --build --abort-on-container-exit --exit-code-from api-tests
