dce6b830fa2f3787139c1241ac20b26a26ffc236
edwin o <enginebox2012@gmail.com>
2022-01-14 14:24:54 +0100

    Remove unnedded.

--- tests/api/mines.api.test.ts
+++ tests/api/mines.api.test.ts
@@ -1,5 +1,4 @@
 import { createApiTestEnvironment } from '../environments'
-import { MongodbSpies, mockClient } from '../../__mocks__/mongodb';
 
 const environment = createApiTestEnvironment();
 environment.initialize();
@@ -7,27 +6,15 @@ environment.initialize();
 jest.mock('mongodb');
 
 describe('Mines API Test', () => {
-    const {
-        findSpy,
-    }: MongodbSpies = jest.requireMock('mongodb');
-
-    beforeEach(() => {
-        findSpy.mockClear();
-    });
 
     it('should return 200', async () => {
-
-        //TODO replace fastify.mongodb to mockClient
-
         const response = await environment.server.inject({
             method: 'GET',
-            url: '/v1/alienworlds/mines?land_id=1099512959271'
+            url: '/v1/alienworlds/mines?land_id=1099512958747'
         });
 
-        expect(response.statusCode).toEqual(200);
-        expect(findSpy).toHaveBeenCalledWith({
-            land_id: '1099512959271'
-        });
+        expect(response.statusCode).toEqual(200)
+        expect(response.results[0].miner).toEqual('fakeminer1.wam')
     });
 
 });
