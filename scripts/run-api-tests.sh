ec3e74fb9542458bb906e59a1a28d20d72fae02c
rkamysz <radoslaw.kamysz@gmail.com>
2022-01-21 18:55:22 +0100

    improve api tests runner, add mongo seed (#19)

--- scripts/run-api-tests.sh
+++ scripts/run-api-tests.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
 script="$(dirname $0)/../docker-compose-api-tests.yml"
-docker compose -f $script up --build --abort-on-container-exit --exit-code-from api-tests
+docker compose -p tests -f $script up --build -d
+
+TESTS_RUNNER_CONTAINER_ID=$(docker ps -aqf "name=^tests-api-tests-runner-1$")
+TEST_EXIT_CODE=`docker wait $TESTS_RUNNER_CONTAINER_ID`
+
+# Log tests results
+docker logs $TESTS_RUNNER_CONTAINER_ID
+
+# Clean
+docker-compose -p tests kill
+docker-compose -p tests rm -f
+
+exit $TEST_EXIT_CODE
