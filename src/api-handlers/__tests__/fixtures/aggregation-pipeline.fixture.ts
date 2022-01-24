11ee9c9a1cbb6ee9c726877681ddca5149b20b2d
rkamysz <radoslaw.kamysz@gmail.com>
2022-01-24 18:26:50 +0100

    write unit tests for mineluck

---
+++ src/api-handlers/__tests__/fixtures/aggregation-pipeline.fixture.ts
@@ -0,0 +1,96 @@
+export const pipelineWithoutBlockTimestamps = [
+  { $match: {} },
+  {
+    $group: {
+      _id: '$miner',
+      total_luck: { $sum: '$params.luck' },
+      total_mines: { $sum: 1 },
+      planets: { $addToSet: '$planet_name' },
+      bag_items: { $addToSet: '$bag_items' },
+    },
+  },
+  { $match: { total_luck: { $gt: 0 } } },
+  {
+    $addFields: {
+      tools: { $arrayElemAt: ['$bag_items', 0] },
+      avg_luck: { $divide: ['$total_luck', '$total_mines'] },
+    },
+  },
+  {
+    $lookup: {
+      from: 'assets',
+      localField: 'tools',
+      foreignField: 'asset_id',
+      as: 'rarities',
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      total_luck: 1,
+      avg_luck: 1,
+      total_mines: 1,
+      planets: 1,
+      tools: 1,
+      rarities: {
+        $map: {
+          input: '$rarities',
+          as: 'asset',
+          in: '$$asset.data.immutable_serialized_data.rarity',
+        },
+      },
+    },
+  },
+];
+
+export const pipelineWithBlockTimestamps = (from: Date, to: Date) => [
+  {
+    $match: {
+      block_timestamp: {
+        $gte: from,
+        $lt: to,
+      },
+    },
+  },
+  {
+    $group: {
+      _id: '$miner',
+      total_luck: { $sum: '$params.luck' },
+      total_mines: { $sum: 1 },
+      planets: { $addToSet: '$planet_name' },
+      bag_items: { $addToSet: '$bag_items' },
+    },
+  },
+  { $match: { total_luck: { $gt: 0 } } },
+  {
+    $addFields: {
+      tools: { $arrayElemAt: ['$bag_items', 0] },
+      avg_luck: { $divide: ['$total_luck', '$total_mines'] },
+    },
+  },
+  {
+    $lookup: {
+      from: 'assets',
+      localField: 'tools',
+      foreignField: 'asset_id',
+      as: 'rarities',
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      total_luck: 1,
+      avg_luck: 1,
+      total_mines: 1,
+      planets: 1,
+      tools: 1,
+      rarities: {
+        $map: {
+          input: '$rarities',
+          as: 'asset',
+          in: '$$asset.data.immutable_serialized_data.rarity',
+        },
+      },
+    },
+  },
+];
---
+++ src/api-handlers/__tests__/mineluck.unit.test.ts
@@ -0,0 +1,81 @@
+import {
+  createMineLuckPipeline,
+  MineLuck,
+  MineLuckResponse,
+} from '../mineluck';
+import {
+  pipelineWithBlockTimestamps,
+  pipelineWithoutBlockTimestamps,
+} from './fixtures/aggregation-pipeline.fixture';
+import { magorWorldCompleteMineluckDocument } from './fixtures/mineluck-document.fixture';
+import {
+  magorWorldCompleteMineluck,
+  magorWorldWithoutRaritiesMineluck,
+} from './fixtures/mineluck.fixture';
+
+describe('MineLuck Unit tests', () => {
+  it('MineLuck.fromDto should throw an error when DTO is not given', () => {
+    const action = () => {
+      MineLuck.fromDto(null);
+    };
+    expect(action).toThrowError();
+  });
+
+  it('MineLuck.fromDto should create instance of the MineLuck class based on given dto (MineLuckDocument)', () => {
+    const mineluck = MineLuck.fromDto(magorWorldCompleteMineluckDocument);
+    expect(mineluck).toEqual(magorWorldCompleteMineluck);
+  });
+});
+
+describe('MineLuckResponse Unit tests', () => {
+  it('MineLuckResponse.create should throw an error when MineLuck collection is not given', () => {
+    const action = () => {
+      MineLuckResponse.create(null);
+    };
+    expect(action).toThrowError();
+  });
+
+  it('MineLuckResponse "count" should be equal to results.length', () => {
+    expect(MineLuckResponse.create([magorWorldCompleteMineluck]).count).toEqual(
+      1
+    );
+    expect(
+      MineLuckResponse.create([
+        magorWorldCompleteMineluck,
+        magorWorldWithoutRaritiesMineluck,
+      ]).count
+    ).toEqual(2);
+  });
+
+  it('MineLuckResponse.create should create instance of the MineLuckResponse class based on given MineLuck collection', () => {
+    expect(
+      MineLuckResponse.create([
+        magorWorldCompleteMineluck,
+        magorWorldWithoutRaritiesMineluck,
+      ])
+    ).toEqual({
+      results: [magorWorldCompleteMineluck, magorWorldWithoutRaritiesMineluck],
+      count: 2,
+    });
+  });
+});
+
+describe('createMineLuckPipeline Unit tests', () => {
+  it('createMineLuckPipeline should create mine luck aggregation pipeline without block timestamp range when "from" and "to" are not given', () => {
+    expect(createMineLuckPipeline()).toEqual(pipelineWithoutBlockTimestamps);
+  });
+
+  it('createMineLuckPipeline should create mine luck aggregation pipeline with block timestamp range when "from" and "to" are given', () => {
+    const from: string = '2022-01-20T19:29:41.294+00:00';
+    const to: string = '2022-01-21T19:29:41.294+00:00';
+    const fromTimestamp: number = 1642706981000;
+    const toTimestamp: number = 1642793381000;
+
+    expect(createMineLuckPipeline(from, to)).toEqual(
+      pipelineWithBlockTimestamps(
+        new Date(fromTimestamp),
+        new Date(toTimestamp)
+      )
+    );
+  });
+});
