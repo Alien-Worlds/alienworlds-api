2960a8943c422b1997494b8ec6ebbf9d4074b1ab
edwin o <enginebox2012@gmail.com>
2022-01-26 11:22:08 +0100

    Added more test.

--- tests/api/mines.api.test.ts
+++ tests/api/mines.api.test.ts
@@ -14,7 +14,27 @@ describe('Mines API Test', () => {
         });
 
         expect(response.statusCode).toEqual(200)
-        expect(response.results[0].miner).toEqual('fakeminer1.wam')
+        console.log(response.payload);
+        const payload = JSON.parse(response.payload);
+        expect(payload.results[0].miner).toEqual('fakeminer1.wam')
+    });
+
+    it('should return Error if limit is > 5000', async () => {
+        const response = await environment.server.inject({
+            method: 'GET',
+            url: '/v1/alienworlds/mines?limit=6000'
+        });
+
+        expect(response.statusCode).toEqual(500)
+    });
+
+    it('should return Error if wrong sort was provided', async () => {
+        const response = await environment.server.inject({
+            method: 'GET',
+            url: '/v1/alienworlds/mines?sort=FAKE'
+        });
+
+        expect(response.statusCode).toEqual(500)
     });
 
 });
