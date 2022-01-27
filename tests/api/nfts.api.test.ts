84de619e00d9dc13ad605a4afc5e9d4b178a2727
edwin o <enginebox2012@gmail.com>
2022-01-27 11:53:55 +0100

    Testing nft route.

---
+++ tests/api/nfts.api.test.ts
@@ -0,0 +1,37 @@
+import { createApiTestEnvironment } from '../environments'
+
+const environment = createApiTestEnvironment();
+environment.initialize();
+
+jest.mock('mongodb');
+
+describe('Mines API Test', () => {
+
+    it('should return 200', async () => {
+        const response = await environment.server.inject({
+            method: 'GET',
+            url: '/v1/alienworlds/nfts?land_id=1099512961112'
+        });
+
+        expect(response.statusCode).toEqual(200)
+    });
+
+    it('should return Error if limit is > 1000', async () => {
+        const response = await environment.server.inject({
+            method: 'GET',
+            url: '/v1/alienworlds/nfts?limit=2000'
+        });
+
+        expect(response.statusCode).toEqual(500)
+    });
+
+    it('should return Error if wrong sort was provided', async () => {
+        const response = await environment.server.inject({
+            method: 'GET',
+            url: '/v1/alienworlds/nfts?sort=FAKE'
+        });
+
+        expect(response.statusCode).toEqual(500)
+    });
+
+});
