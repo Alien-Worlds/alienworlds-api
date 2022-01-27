db47d6413f1459756979ce25b6968277f3fedab1
edwin o <enginebox2012@gmail.com>
2022-01-27 14:14:24 +0100

    fixing pr concerns

--- tests/api/nfts.api.test.ts
+++ tests/api/nfts.api.test.ts
@@ -1,37 +1,33 @@
-import { createApiTestEnvironment } from '../environments'
+import { createApiTestEnvironment } from '../environments';
 
 const environment = createApiTestEnvironment();
 environment.initialize();
 
-jest.mock('mongodb');
-
 describe('NFTS API Test', () => {
-
-    it('should return 200', async () => {
-        const response = await environment.server.inject({
-            method: 'GET',
-            url: '/v1/alienworlds/nfts?land_id=1099512961112'
-        });
-
-        expect(response.statusCode).toEqual(200)
+  it('should return 200', async () => {
+    const response = await environment.server.inject({
+      method: 'GET',
+      url: '/v1/alienworlds/nfts?land_id=1099512961112',
     });
 
-    it('should return Error if limit is > 1000', async () => {
-        const response = await environment.server.inject({
-            method: 'GET',
-            url: '/v1/alienworlds/nfts?limit=2000'
-        });
+    expect(response.statusCode).toEqual(200);
+  });
 
-        expect(response.statusCode).toEqual(500)
+  it('should return Error if limit is > 1000', async () => {
+    const response = await environment.server.inject({
+      method: 'GET',
+      url: '/v1/alienworlds/nfts?limit=2000',
     });
 
-    it('should return Error if wrong sort was provided', async () => {
-        const response = await environment.server.inject({
-            method: 'GET',
-            url: '/v1/alienworlds/nfts?sort=FAKE'
-        });
+    expect(response.statusCode).toEqual(500);
+  });
 
-        expect(response.statusCode).toEqual(500)
+  it('should return Error if wrong sort was provided', async () => {
+    const response = await environment.server.inject({
+      method: 'GET',
+      url: '/v1/alienworlds/nfts?sort=FAKE',
     });
 
+    expect(response.statusCode).toEqual(500);
+  });
 });
