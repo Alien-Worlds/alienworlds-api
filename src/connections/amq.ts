dbe10fd13ee59757bcbff2cc4411a07f201d217b
rkamysz <radoslaw.kamysz@gmail.com>
2022-02-15 12:21:14 +0100

    refactor amq driver

--- src/connections/amq.ts
+++ src/connections/amq.ts
@@ -1,149 +1,182 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-var-requires */
-const Amqp = require('amqplib');
+import Amqp, { Replies } from 'amqplib';
 
+export enum QueueName {
+  Action = 'action',
+  AlienWorldsBlockRange = 'aw_block_range',
+  RecalcAsset = 'recalc_asset',
+}
+
+export type QueueCallback = (message: Amqp.ConsumeMessage) => void;
+
+export type QueueListener = {
+  queue: string;
+  callback: QueueCallback;
+};
+
+export class AmqNotInitializedError extends Error {
+  constructor() {
+    super('Amq not initialized! Run amq.init() first');
+  }
+}
+
+/**
+ * @class
+ */
 export class Amq {
-  logger: any;
-  channel: any;
-  initialized: boolean;
-  connection_errors: number;
-  max_connection_errors: number;
-  listeners: any;
-  connection_string: string;
+  private logger: Console;
+  private channel: Amqp.ConfirmChannel;
+  private connection: Amqp.Connection;
+  private initialized: boolean;
+  private connectionErrorsCount: number;
+  private maxConnectionErrors: number;
+  private listeners: QueueListener[];
+  private address: string;
 
-  constructor(connection_string) {
-    this.connection_string = connection_string;
+  constructor(address: string, logger: Console) {
+    this.address = address;
     this.initialized = false;
     this.listeners = [];
-    this.connection_errors = 0;
-    this.max_connection_errors = 5;
-
-    // this.logger = require('./logger')('eosdac-amq', config.logger);
-    this.logger = console;
+    this.connectionErrorsCount = 0;
+    this.maxConnectionErrors = 5;
+    this.logger = logger;
   }
 
-  async reconnect() {
+  /**
+   * Connection close handler, reconnect
+   */
+  private handleConnectionClose() {
+    if (this.initialized) {
+      this.logger.warn('Connection closed');
+      this.initialized = false;
+      this.reconnect();
+    }
+  }
+
+  /**
+   * Connection error handler
+   *
+   * @param {Error} error
+   */
+  private handleConnectionError(error: Error) {
+    if (error.message !== 'Connection closing') {
+      if (this.connectionErrorsCount > this.maxConnectionErrors) {
+        this.logger.error('Connection Error', { e: error });
+      } else {
+        this.logger.warn('Connection Error', { e: error });
+      }
+      this.initialized = false;
+      this.reconnect();
+    }
+  }
+
+  /**
+   * Reconnect
+   *
+   * @private
+   * @async
+   */
+  private async reconnect() {
     if (this.listeners.length && !this.initialized) {
       this.logger.info(`Reloading connection with listeners`);
 
       try {
         await this.init();
-      } catch (e) {
-        this.connection_errors++;
-
-        const retry_ms = Math.pow(this.connection_errors, 2) * 1000;
-        const log_fn =
-          this.connection_errors > this.max_connection_errors
-            ? this.logger.error
-            : this.logger.warn;
-        log_fn(
-          `${this.connection_errors.toString()} failed connection attempts, retrying in ${Math.floor(
-            retry_ms / 1000
-          ).toString()}s`,
-          { connection_errors: this.connection_errors }
+        this.logger.info(`Adding listeners`);
+        this.listeners.forEach(({ queue, callback }) =>
+          this.listen(queue, callback)
         );
-
-        setTimeout(() => this.reconnect(), retry_ms);
-
-        return;
+      } catch (e) {
+        this.connectionErrorsCount++;
+        const retryMs = Math.pow(this.connectionErrorsCount, 2) * 1000;
+        setTimeout(() => this.reconnect(), retryMs);
       }
-
-      this.logger.info(`Adding listeners`);
-      this.listeners.forEach(({ queue_name, cb }) => {
-        this.listen(queue_name, cb);
-      });
     }
   }
 
-  async init() {
-    const conn = await Amqp.connect(this.connection_string);
+  /**
+   * Initialize amqp driver
+   *
+   * @async
+   */
+  public async init(): Promise<void> {
+    this.connection = await Amqp.connect(this.address);
 
-    const channel = await conn.createConfirmChannel();
-
-    // channel.assertQueue('block_range', {durable: true});
-    // channel.assertQueue('contract_row', {durable: true});
-    // channel.assertQueue('permission_link', {durable: true});
-    // channel.assertQueue('trace', {durable: true});
-    channel.assertQueue('action', { durable: true });
-    channel.assertQueue('aw_block_range', { durable: true });
-    channel.assertQueue('recalc_asset', { durable: true });
-
-    this.channel = channel;
+    this.channel = await this.connection.createConfirmChannel();
+    this.channel.assertQueue(QueueName.Action, { durable: true });
+    this.channel.assertQueue(QueueName.AlienWorldsBlockRange, {
+      durable: true,
+    });
+    this.channel.assertQueue(QueueName.RecalcAsset, { durable: true });
     this.initialized = true;
-    this.connection_errors = 0;
+    this.connectionErrorsCount = 0;
 
-    this.logger.info(`Connected to AMQ ${this.connection_string}`);
+    this.logger.info(`Connected to AMQ ${this.address}`);
 
-    conn.on('error', err => {
-      if (err.message !== 'Connection closing') {
-        const log_fn =
-          this.connection_errors > this.max_connection_errors
-            ? this.logger.error
-            : this.logger.warn;
-
-        log_fn('Connection Error', { e: err });
-        this.initialized = false;
-        this.reconnect();
-      }
-    });
-    conn.on('close', () => {
-      this.logger.warn('Connection closed');
-      this.initialized = false;
-      this.reconnect();
-    });
+    this.connection.on('error', this.handleConnectionError);
+    this.connection.on('close', this.handleConnectionClose);
   }
 
-  async send(queue_name, msg) {
-    return new Promise((resolve, reject) => {
-      if (!Buffer.isBuffer(msg)) {
-        msg = Buffer.from(msg);
-      }
-
-      const return_fn = (err, ok) => {
-        if (err !== null) reject(err);
-        else resolve(ok);
-      };
-
-      if (!this.initialized) {
-        this.init().then(() => {
-          this.channel.sendToQueue(queue_name, msg, {}, return_fn);
-        });
-      } else {
-        this.channel.sendToQueue(queue_name, msg, {}, return_fn);
-      }
-    });
-  }
-
-  async listen(queue_name, cb) {
+  /**
+   * Send a single message with the content given as a buffer to the specific queue named, bypassing routing.
+   *
+   * @param {string} queue
+   * @param {Buffer} message
+   * @param {Function} callback
+   */
+  public send(
+    queue: string,
+    message: Buffer,
+    callback?: (err: Error, ok: Replies.Empty) => void
+  ): void {
     if (!this.initialized) {
-      await this.init();
+      throw new AmqNotInitializedError();
     }
-
-    this.channel.prefetch(1);
-    // await this.channel.assertQueue(queue_name, {durable: true})
-    this.listeners.push({ queue_name, cb });
-
-    this.channel.consume(queue_name, cb, { noAck: false });
+    this.channel.sendToQueue(queue, message, {}, callback);
   }
 
-  async ack(job) {
+  /**
+   * Set up a listener for the specified queue.
+   *
+   * @async
+   * @param {string} queue - queue name
+   * @param {QueueCallback} callback - queue callback
+   */
+  public async listen(queue: string, callback: QueueCallback): Promise<void> {
     if (!this.initialized) {
-      await this.init();
+      throw new AmqNotInitializedError();
+    }
+    this.channel.prefetch(1);
+    this.listeners.push({ queue, callback });
+    this.channel.consume(queue, callback, { noAck: false });
+  }
+
+  /**
+   * Acknowledge the given message, or all messages up to and including the given message.
+   *
+   * @param {Amqp.Message} job
+   */
+  public ack(job): void {
+    if (!this.initialized) {
+      throw new AmqNotInitializedError();
     }
     try {
-      return this.channel.ack(job);
+      this.channel.ack(job);
     } catch (e) {
       this.logger.error(`Failed to ack job`, e);
-      // failure to ack isnt a problem, all jobs are idempotent
     }
   }
 
-  async reject(job) {
+  /**
+   * Reject a message.
+   *
+   * @param {Amqp.Message} job
+   */
+  public reject(job: Amqp.Message): void {
     if (!this.initialized) {
-      await this.init();
+      throw new AmqNotInitializedError();
     }
     try {
-      return this.channel.reject(job, true);
+      this.channel.reject(job, true);
     } catch (e) {
       this.logger.error(`Failed to reject job`);
     }
