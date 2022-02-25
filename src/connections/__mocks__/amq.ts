3971eb364ab37e00d7b406d65df9608eecb1ea8d
rkamysz <radoslaw.kamysz@gmail.com>
2022-02-25 22:01:50 +0100

    refactor code, add tests

---
+++ src/connections/__mocks__/amq.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
+
+export class Amq {
+  constructor(url: string, logger: Console) {}
+
+  reject(...args: any[]) {}
+  ack(...args: any[]) {}
+  addListener(...args: any[]) {}
+  send(...args: any[]) {}
+  init(...args: any[]) {}
+}
