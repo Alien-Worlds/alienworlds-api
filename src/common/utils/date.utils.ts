89e7305af42abc401a86747ebdcbc8152b8c8f39
rkamysz <radoslaw.kamysz@gmail.com>
2022-07-07 18:20:57 +0200

    revert parseDateToMs changes

--- src/common/utils/date.utils.ts
+++ src/common/utils/date.utils.ts
@@ -2,15 +2,22 @@ export const parseDateToMs = (fullStr: string) => {
   const [fullDate] = fullStr.split('.');
   const [dateStr, timeStr] = fullDate.split('T');
   const [year, month, day] = dateStr.split('-');
-  const [hourStr, minuteStr, secondStr] = timeStr.split(':');
+  let hourStr = '0';
+  let minuteStr = '0';
+  let secondStr = '0';
+
+  if (timeStr) {
+    [hourStr, minuteStr, secondStr] = timeStr.split(':');
+  }
 
   const dt = new Date();
-  dt.setUTCFullYear(Number(year));
-  dt.setUTCMonth(Number(month) - 1);
-  dt.setUTCDate(Number(day));
-  dt.setUTCHours(Number(hourStr));
-  dt.setUTCMinutes(Number(minuteStr));
-  dt.setUTCSeconds(Number(secondStr));
+  dt.setUTCFullYear(+year);
+  dt.setUTCMonth(+month - 1);
+  dt.setUTCDate(+day);
+  dt.setUTCHours(+hourStr);
+  dt.setUTCMinutes(+minuteStr);
+  dt.setUTCSeconds(+secondStr);
+  dt.setUTCMilliseconds(0);
 
   return dt.getTime();
 };
